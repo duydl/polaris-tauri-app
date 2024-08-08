28ecbb0f923324b9794324b2f200fc7bd60579d9
duydl <duy.dole.00ece@gmail.com>
2024-08-08 18:04:10 +0700

    Start with developer console opened

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,4 @@
+
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
@@ -6,10 +7,15 @@
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
-
+use tauri::Manager;
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
+      .setup(|app| {
+        #[cfg(debug_assertions)]
+        app.get_window("main").unwrap().open_devtools(); // `main` is the first window from tauri.conf.json without an explicit label
+        Ok(())
+      })
+      .invoke_handler(tauri::generate_handler![greet])
+      .run(tauri::generate_context!())
+      .expect("error while running tauri application");
+ }
