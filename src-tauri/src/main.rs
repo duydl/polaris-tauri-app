6ba72beef2112bfc71d293b61154344b57f0c8a7
duydl <duy.dole.00ece@gmail.com>
2024-08-09 18:35:19 +0700

    Misc adjustment for build/dev

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
 
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -11,8 +11,12 @@ use tauri::Manager;
 fn main() {
     tauri::Builder::default()
       .setup(|app| {
-        #[cfg(debug_assertions)]
-        app.get_window("main").unwrap().open_devtools(); // `main` is the first window from tauri.conf.json without an explicit label
+        // #[cfg(debug_assertions)]
+        {
+            let window = app.get_window("main").unwrap();
+            // window.open_devtools();
+            // window.close_devtools();
+        }
         Ok(())
       })
       .invoke_handler(tauri::generate_handler![greet])
